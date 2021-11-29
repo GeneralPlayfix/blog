1b5524b5f314e36b761c5fc2522b8a1f9914063f
GeneralPlayfix <81032503+GeneralPlayfix@users.noreply.github.com>
2021-11-29 15:14:13 +0100

    exo9

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -2,12 +2,15 @@
 
 namespace App\Controller;
 
+use App\Entity\Contact;
+use App\Form\ContactType;
 use App\Repository\ContactRepository;
 use phpDocumentor\Reflection\Types\String_;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
+
 class ContactController extends AbstractController
 {
     private $contactRepository;
@@ -18,32 +21,24 @@ class ContactController extends AbstractController
     /**
      * @Route("/contact", name="contactWithoutNothing")
      */
-    public function index(): Response
+    public function index(Request $request): Response
     {
-        return $this->render('contact/index.html.twig', [
+        $contact = new Contact();
+        $form = $this->createForm(ContactType::class, $contact);
+        return $this->renderForm('contact/index.html.twig', [
             'test' => $this->contactRepository->findAll(),
+            'form'=> $form,
     ]);
     }
 
     /**
      * @Route("/contact/{id}", name="contactById")
      */
-    public function contactById(string $id): Response
+
+    public function contactById(int $id): Response
     {
         return $this->render('contact/index.html.twig', [
             'user' => $this->contactRepository->find($id),
-        ]);
-    }
-
-    /**
-     * @Route("/contact/{contactCity}", name="contact")
-     */
-    public function contact(Request $request, string $contactCity): Response
-    {
-        $name = $request->query->get('name');
-        return $this->render('contact/index.html.twig', [
-            'contactCity' => $contactCity,
-            'name'=>$name
         ]);
     }
 }
--- src/Controller/CreateArticleController.php
+++ src/Controller/CreateArticleController.php
@@ -1,6 +1,10 @@
 <?php
 
 namespace App\Controller;
+use App\Entity\Article;
+use App\Form\ArticleType;
+use App\Repository\ArticleRepository;
+use Symfony\Component\HttpFoundation\Request;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -8,13 +12,27 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class CreateArticleController extends AbstractController
 {
+    private $articleRepository;
+
+    public function __construct(ArticleRepository $articleRepository){
+        $this->articleRepository = $articleRepository;
+    }
     /**
      * @Route("/create_article", name="create_article")
      */
-    public function index(): Response
+    public function index(Request $request): Response
     {
-        return $this->render('create_article/index.html.twig', [
-            'controller_name' => 'CreateArticleController',
+        $article = new Article();
+        $form = $this->createForm(ArticleType::class, $article);
+        $form-> handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($form->getData());
+            $entityManager->flush();
+            dump('Posté en base');
+        }
+        return $this->renderForm('create_article/index.html.twig', [
+            'form'=> $form,
         ]);
     }
 }
